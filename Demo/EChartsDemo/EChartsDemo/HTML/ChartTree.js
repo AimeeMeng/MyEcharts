dd0318a0bb686153f72a021f6fbb0bad0ef43ecf
AimeeMeng <2020583296@qq.com>
2018-07-14 17:54:18 +0800

    树图

--- Demo/EChartsDemo/EChartsDemo/HTML/ChartTree.js
+++ Demo/EChartsDemo/EChartsDemo/HTML/ChartTree.js
@@ -4,6 +4,7 @@
     LoadChartTree3();
     LoadChartTreemap();
     LoadChartSunBurst();
+    LoadTestChart();
 });
 
 var _source = null;
@@ -601,4 +602,83 @@ var LoadChartSunBurst = function () {
     window.onresize = function () {
         Chart.resize();
     };
+}
+
+var LoadTestChart = function ()
+{
+    myChart.showLoading();
+    $.get('http://www.echarts.baidu.com/data/asset/data/les-miserables.gexf', function (xml) {
+        myChart.hideLoading();
+
+        var graph = echarts.dataTool.gexf.parse(xml);
+        var categories = [];
+        for (var i = 0; i < 9; i++) {
+            categories[i] = {
+                name: '类目' + i
+            };
+        }
+        graph.nodes.forEach(function (node) {
+            node.itemStyle = null;
+            node.value = node.symbolSize;
+            node.symbolSize /= 1.5;
+            node.label = {
+                normal: {
+                    show: node.symbolSize > 30
+                }
+            };
+            node.category = node.attributes.modularity_class;
+        });
+        option = {
+            title: {
+                text: 'Les Miserables',
+                subtext: 'Default layout',
+                top: 'bottom',
+                left: 'right'
+            },
+            tooltip: {},
+            legend: [{
+                // selectedMode: 'single',
+                data: categories.map(function (a) {
+                    return a.name;
+                })
+            }],
+            animationDuration: 1500,
+            animationEasingUpdate: 'quinticInOut',
+            series: [
+                {
+                    name: 'Les Miserables',
+                    type: 'graph',
+                    layout: 'none',
+                    data: graph.nodes,
+                    links: graph.links,
+                    categories: categories,
+                    roam: true,
+                    focusNodeAdjacency: true,
+                    itemStyle: {
+                        normal: {
+                            borderColor: '#fff',
+                            borderWidth: 1,
+                            shadowBlur: 10,
+                            shadowColor: 'rgba(0, 0, 0, 0.3)'
+                        }
+                    },
+                    label: {
+                        position: 'right',
+                        formatter: '{b}'
+                    },
+                    lineStyle: {
+                        color: 'source',
+                        curveness: 0.3
+                    },
+                    emphasis: {
+                        lineStyle: {
+                            width: 10
+                        }
+                    }
+                }
+            ]
+        };
+
+        myChart.setOption(option);
+    }, 'xml');
 }
